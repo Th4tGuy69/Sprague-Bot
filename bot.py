f138a04ebfdc22416f3492b6cfa604057574bcbb
Th4tGuy69 <thatguytf3@outlook.com>
2019-09-08 12:58:40 -0700

    DM to User on join

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@
 #TODO: Read token and prefix from json file
 #TODO: Create a tool to read sprague classes from a file, create categories and chats for them, and roles for each class
 #TODO: Censorship warning system
-#TODO: Connect to a student database to auto-assign roles and nick-names?
-#      (Doubt we would get access to that if it does exist)
+#TODO: Connect to a student database to auto-assign roles and nick-names? Same system that does schedule changes with
+#      student ID, first letter of last name, email, etc. (Doubt we would get access... unless..?)
 #TODO: Try to make this ass professional as possible :)
 #---
 
@@ -31,9 +31,20 @@ async def on_ready():
     print('Ready to use\n')
 
 
+@client.event #Sends new users a message on joining the guild, TODO:Test
+async def on_member_join(member):
+    embed = discord.Embed(title='*beep boop*', type='rich', description='Bot text')
+    await member.send(embed=embed)
+
+
 @client.event
 async def on_message(message):
-    if message.content.startswith(startsWith):
-        print('hey')
+    if message.author == client.user:
+        return
+    elif message.content.startswith(startsWith):
+        if 'join' in message.content:
+            embed = discord.Embed(title='*beep boop*', type='rich', description='Bot text')
+            await message.author.send(embed=embed)
+
 
 client.run(token)
