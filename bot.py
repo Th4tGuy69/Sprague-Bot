2a19a627fc552285867c8b12736b72d6608ccfa9
Th4tGuy69 <thatguytf3@outlook.com>
2019-09-12 21:46:49 -0700

    Announcemnts poster (needs to be scheduled)

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@
 #TODO: Try to make this ass professional as possible :) - IN PROGRESS
 #---
 
+import re
 import sys
 import json
 import discord
@@ -13,12 +14,23 @@ import requests
 from bs4 import BeautifulSoup
 from discord.ext import commands
 
+
+announcements = []
 page = requests.get('http://spragueannouncements.blogspot.com/')
 
-soup = BeautifulSoup(page.content, 'lxml')
 
-with open('pagesource.html', 'w', encoding='utf-8') as f_out:
-    f_out.write(soup.prettify())
+soup = BeautifulSoup(page.content, 'lxml')
+whole = soup.find(class_='post-body entry-content')
+class_ = whole.find_all('div')
+class_.pop(0)
+for div in class_:
+    if len(div.text) > 3:
+        text = div.text
+        if '/' in text:
+            text = text[:-4]
+        text = re.sub(r'\s+',' ',text)
+
+        announcements.append(text)
 
 
 with open('info.json', 'r') as json_file:
@@ -32,12 +44,14 @@ with open('info.json', 'r') as json_file:
 client = commands.Bot(command_prefix=prefix)
 
 
-embed = discord.Embed(title='Today\'s Announcements', type='rich', url='http://spragueannouncements.blogspot.com/', description='———', color=0xf04923)
+embed = discord.Embed(title='Today\'s Announcements', type='rich', url='http://spragueannouncements.blogspot.com/', color=0xf04923)
 embed.set_author(name='Sprague Bot', url='https://github.com/Th4tGuy69/Sprague-Bot', icon_url='http://spraguehs.com/images/oly-logos/victory-o-orange.svg')
-embed.add_field(name=announemnt, value='———', inline=False)
-embed.add_field(name=announemnt, value='———', inline=False)
-embed.add_field(name=announemnt, value='———', inline=False)
-embed.set_footer(text='Make it a great day!')
+for announcement in announcements:
+    try:
+        embed.add_field(name='———', value=announcement, inline=False)
+    finally:
+        pass
+embed.set_footer(text=dayType + ' | Make it a great day!')
 
 
 @client.event
@@ -59,6 +73,9 @@ async def on_member_join(member):
 async def on_message(message):
     if message.author == client.user:
         return
+    elif message.content.startswith(prefix):
+        if 'ann' in message.content:
+            await message.channel.send(embed=embed)
 
 
 client.run(token)
