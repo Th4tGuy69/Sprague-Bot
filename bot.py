d53f697e3acad34202524231aa80093b8e51994a
Th4tGuy69 <thatguytf3@outlook.com>
2019-09-13 23:17:39 -0700

    Posts announcements every weekday at 7AM

--- bot.py
+++ bot.py
@@ -1,38 +1,31 @@
-#---
-#TODO: Scrape announcenments using BeautifulSoup and post into #announcements 
-#TODO: Censorship warning system
-#TODO: Connect to a student database to auto-assign roles and nick-names? Same system that does schedule changes with
+# ---
+# TODO: Censorship warning system
+# TODO: Connect to a student database to auto-assign roles and nick-names? Same system that does schedule changes with
 #      student ID, first letter of last name, email, etc. (Doubt we would get access... unless..?)
-#TODO: Try to make this ass professional as possible :) - IN PROGRESS
-#---
+# TODO: Try to make this ass professional as possible :) - IN PROGRESS
+# ---
 
 import re
 import sys
 import json
 import discord
 import requests
+import datetime as dt
 from bs4 import BeautifulSoup
 from discord.ext import commands
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 
+emojiA = 'https://i.imgur.com/dmTKeTi.png'
+emojiB = 'https://i.imgur.com/oLJnbDL.png'
+emojiAB = 'https://i.imgur.com/dVOtgZq.png'
 announcements = []
-page = requests.get('http://spragueannouncements.blogspot.com/')
+date = ''
+dayType = ''
+global embed
 
 
-soup = BeautifulSoup(page.content, 'lxml')
-whole = soup.find(class_='post-body entry-content')
-class_ = whole.find_all('div')
-class_.pop(0)
-for div in class_:
-    if len(div.text) > 3:
-        text = div.text
-        if '/' in text:
-            text = text[:-4]
-        text = re.sub(r'\s+',' ',text)
-
-        announcements.append(text)
-
-
+# Grab bot token and prefix from file, TODO: If we have any actual user commands, make the prefix changable
 with open('info.json', 'r') as json_file:
     data = json.load(json_file)
     for j in data['info']:
@@ -44,16 +37,6 @@ with open('info.json', 'r') as json_file:
 client = commands.Bot(command_prefix=prefix)
 
 
-embed = discord.Embed(title='Today\'s Announcements', type='rich', url='http://spragueannouncements.blogspot.com/', color=0xf04923)
-embed.set_author(name='Sprague Bot', url='https://github.com/Th4tGuy69/Sprague-Bot', icon_url='http://spraguehs.com/images/oly-logos/victory-o-orange.svg')
-for announcement in announcements:
-    try:
-        embed.add_field(name='———', value=announcement, inline=False)
-    finally:
-        pass
-embed.set_footer(text=dayType + ' | Make it a great day!')
-
-
 @client.event
 async def on_ready():
     print('\nLogged in as: ', client.user.name)
@@ -63,9 +46,11 @@ async def on_ready():
     print('Ready to use\n')
 
 
-@client.event #Sends new users a message on joining the guild, TODO:Test and replace embed with something more professional
+# Sends new users a message on joining the guild, TODO:Test and replace embed with something more professional
+@client.event
 async def on_member_join(member):
-    embed = discord.Embed(title='*beep boop*', type='rich', description='Bot text')
+    embed = discord.Embed(title='*beep boop*',
+                          type='rich', description='Bot text')
     await member.send(embed=embed)
 
 
@@ -73,9 +58,64 @@ async def on_member_join(member):
 async def on_message(message):
     if message.author == client.user:
         return
-    elif message.content.startswith(prefix):
-        if 'ann' in message.content:
-            await message.channel.send(embed=embed)
+
+
+# Web crawler grabs announcements, modifys, then posts to id=619772443116175370
+async def postAnnouncements():
+    announcements.clear()
+
+    date = str(dt.date.today()).split('-')
+
+    if dt.date.today().weekday() is 1 or 3:
+        dayType = 'an **A** day'
+    elif dt.date.today().weekday() is 2 or 4:
+        dayType = 'a **B** day'
+    elif dt.date.today().weekday() is 0:
+        dayType = 'an **A/B** day'
+
+    # Web crawler
+    page = requests.get('http://spragueannouncements.blogspot.com/')
+
+    soup = BeautifulSoup(page.content, 'lxml')
+    whole = soup.find(class_='post-body entry-content')
+    class_ = whole.find_all('div')
+    class_.pop(0)
+    for div in class_:
+        if len(div.text) > 3:
+            text = div.text
+            if '/' in text:
+                text = text[:-4]
+            text = re.sub(r'\s+', ' ', text)
+
+            announcements.append(text)
+
+    # Embed setup
+    embed = discord.Embed(title='Announcements for **{}/{}/{}**, {}.'.format(date[1].replace(
+        '0', ''), date[2], date[0][2:], dayType), type='rich', url='http://spragueannouncements.blogspot.com/', color=0xf04923)
+    embed.set_author(name='Sprague Bot', url='https://github.com/Th4tGuy69/Sprague-Bot',
+                     icon_url='http://spraguehs.com/images/oly-logos/Victory%20O%20Orange.png')
+    embed.set_footer(text='Remember to make it a great day, everybody!')
+    if dt.date.today().weekday() is 1 or 3:
+        embed.set_thumbnail(url=emojiA)
+    elif dt.date.today().weekday() is 2 or 4:
+        embed.set_thumbnail(url=emojiB)
+    elif dt.date.today().weekday() is 0:
+        embed.set_thumbnail(url=emojiAB)
+    for announcement in announcements:
+        try:
+            embed.add_field(name='⸻\t\t\t⸻\t\t\t⸻\t\t\t⸻\t\t\t⸻',
+                            value=announcement, inline=False)
+        finally:
+            pass
+
+    await client.get_channel(619772443116175370).send(embed=embed)
+
+
+# Scheduled posting of announcements
+scheduler = AsyncIOScheduler()
+scheduler.add_job(postAnnouncements, trigger='cron',
+                  day_of_week='mon-fri', hour=7)
+scheduler.start()
 
 
 client.run(token)
